3d117a8cd9fa74369cfbb599a3f4deb680725a56
Sanna Siljebäck <sannasiljeback@gmail.com>
2023-11-04 22:29:49 +0100

    got an object in an object to be printed out

--- js/main.js
+++ js/main.js
@@ -1,43 +1,55 @@
 //variabler med olika url som man sen kan sätta in i fetchfunctionen för att slippa göra flera olika funktioner
+
+//url från https://sunrisesunset.io/api/ hemsidan
 const sweUrl = "https://api.sunrisesunset.io/json?lat=59.329&lng=18.068";
-const espUrl = "https://api.sunrisesunset.io/json?lat=41.385&lng=2.173";
+/* const espUrl = "https://api.sunrisesunset.io/json?lat=41.385&lng=2.173";
 const argUrl = "https://api.sunrisesunset.io/json?lat=-34.607&lng=-58.437";
 const caUrl = "https://api.sunrisesunset.io/json?lat=45.420&lng=-75.690";
 const jpnUrl = "https://api.sunrisesunset.io/json?lat=35.684&lng=139.774";
 const nzlUrl = "https://api.sunrisesunset.io/json?lat=-41.288&lng=174.777";
-const zaUrl = "https://api.sunrisesunset.io/json?lat=-33.928&lng=18.417";
+const zaUrl = "https://api.sunrisesunset.io/json?lat=-33.928&lng=18.417"; */
+
+//städerna för urln
+//sweUrl = stockholm
+//espUrl = barcelona
+//argUrl = buenos aires
+//caUrl = ottawa
+//jpnUrl = tokyo
+//nzlUrl = wellington
+//zaUrl = kapstaden
 
 
+//parametrar i apin
+//allt ligger inuti results
+//sunrise
+//sunset
+//dusk
+//dawn
+//golden_hour
+//day_length
 
-//gamla sättet att skriva på
-/* fetch("https://date.nager.at/api/v3/publicholidays/2023/SE")
-.then(res => res.json())
-.then(data => console.log(data)) */
 
-//samma som den över men nyare sättet
-async function fetchData(url) {
-    try {
-        let response = await fetch(url);
+//hämtar alla idn
+let divContainer = document.getElementById('container');
 
-        if (response.ok === false) {
-            throw new Error(`HTTP error code: ${respons.status}, HTTP error message: ${response.statusText}`);
-        }
+let divCities = document.getElementById('div-cities');
+let datum = document.getElementById('datum');
+let huvudstad = document.getElementById('huvudstad');
+let cities = document.getElementById('cities');
+let rightBtn = document.getElementById('right-btn');
+let leftBtn = document.getElementById('left-btn');
 
-        let data = await response.json();
-        console.log(data);
-
-    } catch (error) {
-        console.log(error);
-        document.getElementById('error').innerHTML = "OPPS OPPS";
-    }
-
-    /* return data; */ //behövs en return här?
-    
-};
-
-function printData() {
-
-};
+let divInfo = document.getElementById('divInfo');
+let divDayLength = document.getElementById('div-dayLength');
+let pDayLenght = document.getElementById('day-length');
+let divSunrise = document.getElementById('div-sunrise');
+let pSunrise = document.getElementById('sunrise');
+let pDawn = document.getElementById('dawn');
+let divSunset = document.getElementById('div-sunset');
+let pSunset = document.getElementById('sunset');
+let pdusk = document.getElementById('dusk');
+let divGoldenHour = document.getElementById('div-goldenHour');
+let pGoldenHour = document.getElementById('pGoldenHour');
 
 
 //få fram dagens datum
@@ -52,12 +64,55 @@ let currentDate = `${day}/${month}-${year}`;
 document.getElementById('datum').innerHTML = currentDate;
 
 
+
+//gamla sättet att skriva på
+/* fetch("https://date.nager.at/api/v3/publicholidays/2023/SE")
+.then(res => res.json())
+.then(data => console.log(data)) */
+
+
+//samma som den över men nyare sättet
+async function fetchData(url) {
+    try {
+        let response = await fetch(url);
+
+        if (response.ok === false) {
+            throw new Error(`HTTP error code: ${response.status}, HTTP error message: ${response.statusText}`);
+        }
+
+        let data = await response.json();
+        
+
+        //hittar alla objekt och sen ett specifikt i det objektet, här hittar den sunrise inuti results
+        let resultsObject = data.results; // får ut hela objektet
+        console.log(resultsObject); 
+
+        let sunriseValue = resultsObject.sunrise; // får ut sunrise objektet
+        console.log(sunriseValue); 
+
+        pSunrise.innerHTML = sunriseValue;
+
+
+    
+
+    } catch (error) {
+        console.log(error);
+        document.getElementById('error').innerHTML = "OPPS OPPS";
+    }
+
+    /* return data; */ //behövs en return här?
+    
+};
+
+
+
+
 //fetchar datan med alla våra städer i
 fetchData(sweUrl);
-fetchData(espUrl);
+/* fetchData(espUrl);
 fetchData(argUrl);
 fetchData(caUrl);
 fetchData(jpnUrl);
 fetchData(nzlUrl);
-fetchData(zaUrl);
+fetchData(zaUrl); */
 
